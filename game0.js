535d7fffb9e4c7f3b5bb2ed18d4febeede679612
axuhs <axuhs@users.noreply.github.com>
2018-03-18 12:17:32 -0400

    additional feature

--- game0.js
+++ game0.js
@@ -127,6 +127,20 @@ The user moves a cube around the board trying to knock balls into a cone
       }})
 			scene.add(npc);
 
+	//Angel
+			npc2 = createNPC2(0xffb6c1);
+			npc2.position.set(-30,5,-40);
+			this.__dirtyPosition = true;
+			npc2.addEventListener('collision',
+					function addNPC2Scores (other_object){
+						if (other_object==avatar){
+							console.log( "Avatar hits the NPC2");
+							soundEffect('good.wav');
+							gameState.health += 1;  // add one to the score
+						}
+					})
+					scene.add(npc2);		
+
       var wall = createWall(0xffaa00,50,3,1);
       wall.position.set(10,0,10);
       scene.add(wall);
@@ -268,6 +282,15 @@ The user moves a cube around the board trying to knock balls into a cone
 		mesh.castShadow = true;
 		return mesh;
 	}
+
+	function createNPC2(color){
+		var geometry = new THREE.TorusKnotGeometry( 2.5, 0.75, 25, 4 );
+		var material = new THREE.MeshLambertMaterial( { color: color } );
+		mesh = new Physijs.BoxMesh( geometry, material,0 );
+		//mesh = new Physijs.BoxMesh( geometry, material,0 );
+		mesh.castShadow = true;
+		return mesh;
+	}//Angel
 
   function createWall(color,w,h,d){
     var geometry = new THREE.BoxGeometry( w, h, d);
