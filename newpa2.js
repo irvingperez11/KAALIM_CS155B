3d57bee8a5f6692d0b1c3485e27f260b3495ff52
Irving Perez <irvingperez@Irvings-MacBook-Pro.local>
2018-03-19 20:29:05 -0400

    Fix issue where the rotation would continue on even after you released.

--- newpa2.js
+++ newpa2.js
@@ -460,21 +460,20 @@ The user moves a cube around the board trying to knock balls into a cone
 			case "s": controls.bwd = true; break;
 			case "a": controls.left = true; break;
 			case "d": controls.right = true; break;
-			case "t": controls.up = true; break;
+			case "r": controls.up = true; break;
 			case "f": controls.down = true; break;
 			case "m": controls.speed = 30; break;
-
-      case " ": controls.fly = true;
+      case " ": controls.fly = true
           console.log("space!!");
           break;
+				case "p": avatar.rotation.set(0,0,0); avatar.__dirtyRotation = true; break;
       case "h": controls.reset = true; break;
 	  case "0": controls.reset2 = true; break;
-		case "r": avatar.rotation.set(0,0,0); avatar.__dirtyRotation = true;
 
 			// switch cameras
 			case "1": gameState.camera = camera; break;
 			case "2": gameState.camera = avatarCam; break;
-      case "3": gameState.camera = edgeCam; break;
+      		case "3": gameState.camera = edgeCam; break;
 			//move the avatar camera around
 			case "ArrowLeft": avatarCam.translateY(1);break;
 			case "ArrowRight": avatarCam.translateY(-1);break;
@@ -490,11 +489,12 @@ The user moves a cube around the board trying to knock balls into a cone
 		//console.log("Keydown:"+event.key);
 		//console.dir(event);
 		switch (event.key){
+
 			case "w": controls.fwd   = false;  break;
 			case "s": controls.bwd   = false; break;
 			case "a": controls.left  = false; break;
 			case "d": controls.right = false; break;
-			case "r": avatar.rotation.set(0,0,0); avatar.__dirtyRotation = true;
+			case "r": controls.up    = false; break;
 			case "f": controls.down  = false; break;
 			case "m": controls.speed = 10; break;
       case " ": controls.fly = false; break;
