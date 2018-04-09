6fcad4e66384595f1e901744c377cc7078a5f3db
Irving Perez <irvingperez@Irvings-MacBook-Pro.local>
2018-04-09 16:34:08 -0400

    Skybox textures and code

--- PA3TestingFile.js
+++ PA3TestingFile.js
@@ -58,6 +58,20 @@ Thank you! Don't forget to comment!!
 			// create the ground and the skybox
 			var ground = createGround('grass.png');
 			scene.add(ground);
+			// creates the shape
+			var geometry = new THREE.CubeGeometry( 250, 250, 250 );
+			var cubeMaterials = [
+					new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "img/nightsky_ft.png" ), side: THREE.DoubleSide }), //front side
+					new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_bk.png' ), side: THREE.DoubleSide }), //back side
+					new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_up.png' ), side: THREE.DoubleSide }), //up side
+					new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_dn.png' ), side: THREE.DoubleSide }), //down side
+					new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_rt.png' ), side: THREE.DoubleSide }), //right side
+					new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_lf.png' ), side: THREE.DoubleSide }) //left side
+			];
+
+var cubeMaterial = new THREE.MeshFaceMaterial( cubeMaterials );
+var cube = new THREE.Mesh( geometry, cubeMaterial );
+scene.add( cube );
 
 			// create the avatar
 			avatarCam = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
